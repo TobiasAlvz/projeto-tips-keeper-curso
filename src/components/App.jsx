cde050240627da6925453b26bd23de444a70aefd
Tobias Alves <tobiasjose12@gmail.com>
2024-04-14 15:55:29 -0300

    Create App.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import Note from "./Note";
+import Footer from "./Footer";
+import "../styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
