ab484339fab2bddaf0c1bdd427805d28e5b5691a
Tobias Alves <tobiasjose12@gmail.com>
2024-04-23 10:35:31 -0300

    Create notes.js

---
+++ src/notes.js
@@ -0,0 +1,29 @@
+const notes = [
+  {
+    id: 1,
+    title: "Nota 1",
+    description: "Descrição da Nota 1",
+  },
+  {
+    id: 2,
+    title: "Nota 2",
+    description: "Descrição da Nota 2",
+  },
+  {
+    id: 3,
+    title: "Nota 3",
+    description: "Descrição da Nota 3",
+  },
+  {
+    id: 4,
+    title: "Nota 4",
+    description: "Descrição da Nota 4",
+  },
+  {
+    id: 5,
+    title: "Nota 5",
+    description: "Descrição da Nota 5",
+  },
+];
+
+export default notes;
