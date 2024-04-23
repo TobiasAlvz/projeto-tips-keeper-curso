2af7d0d31061681752d867d67330f6fc24a78ac7
Tobias Alves <tobiasjose12@gmail.com>
2024-04-23 10:36:50 -0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "../notes";
 import "../styles.css";
+
+function CreatNote(note) {
+  return (
+    <Note key={note.id} title={note.title} description={note.description} />
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Note />
+      {notes.map(CreatNote)}
       <Footer />
     </div>
   );
